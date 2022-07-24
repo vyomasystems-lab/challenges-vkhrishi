c011a92de018e1daea82f3d9ed7d07344774f021
vkhrishi <imhrishi18@gmail.com>
2022-07-24 16:05:50 +0000

    updated

--- level1_design2_fix/test_seq_detect_1011.py
+++ level1_design2_fix/test_seq_detect_1011.py
@@ -11,7 +11,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer
  
 @cocotb.test()
-async def test_seq_bug1(dut):
+async def test1_seq_bug1(dut):
     """Test for seq detection """
  
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
@@ -27,7 +27,7 @@ async def test_seq_bug1(dut):
  
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
-    dut.inp_bit.value = 0
+    dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 0
     await FallingEdge(dut.clk)
@@ -35,16 +35,19 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
- 
-    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 11011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
- 
+    
+    b=1
+    
+    dut._log.info(f'b={b:02} model={b:02} DUT={int(dut.seq_seen.value):02}')
+    assert dut.seq_seen.value == b, "Sequence given 11011, result is incorrect:  {b} != {OUT}, expected value={EXP}".format(
+       b=1, OUT=int(dut.seq_seen.value), EXP=b)
+
 @cocotb.test()
-async def test_seq_bug2(dut):
+async def test2_seq_bug2(dut):
     """Test for seq detection """
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
  
-    dut._log.info(f'Input bit: {dut.inp_bit.value}')
      
     # reset
     dut.reset.value = 1
@@ -60,6 +63,8 @@ async def test_seq_bug2(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
     dut.inp_bit.value = 0
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
@@ -67,4 +72,9 @@ async def test_seq_bug2(dut):
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
  
-    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 101011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
+ 
+    b=1
+    
+    dut._log.info(f'b={b:02} model={b:02} DUT={int(dut.seq_seen.value):02}')
+    assert dut.seq_seen.value == b, "Sequence given 1011011, result is incorrect:  {b} != {OUT}, expected value={EXP}".format(
+       b=1, OUT=int(dut.seq_seen.value), EXP=b)
